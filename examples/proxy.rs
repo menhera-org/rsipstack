ca90d85ba942ebda338555e8f9772694cb18d07c
metastable-void <git@yuka.h09.eu>
2025-10-05 14:43:40 +0900

    flag implementation fix

--- examples/proxy.rs
+++ examples/proxy.rs
@@ -382,6 +382,11 @@ async fn handle_invite(state: AppState, mut tx: Transaction) -> Result<()> {
     inv_req.headers.push_front(via.into());
     if tx.endpoint_inner.option.follow_record_route {
         inv_req.headers.push_front(record_route.clone().into());
+    } else {
+        inv_req.headers.retain(|h| !matches!(
+            h,
+            rsip::Header::RecordRoute(_) | rsip::Header::Route(_)
+        ));
     }
     let key = TransactionKey::from_request(&inv_req, TransactionRole::Client)
         .expect("client_transaction");
@@ -458,6 +463,12 @@ async fn handle_bye(state: AppState, mut tx: Transaction) -> Result<()> {
     let mut inv_req = tx.original.clone();
     let via = tx.endpoint_inner.get_via(None, None)?;
     inv_req.headers.push_front(via.into());
+    if !tx.endpoint_inner.option.follow_record_route {
+        inv_req.headers.retain(|h| !matches!(
+            h,
+            rsip::Header::RecordRoute(_) | rsip::Header::Route(_)
+        ));
+    }
 
     let key = TransactionKey::from_request(&inv_req, TransactionRole::Client)
         .expect("client_transaction");
